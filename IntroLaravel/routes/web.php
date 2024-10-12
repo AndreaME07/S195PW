54b0ed3f54743c4463bd2808c3791f196e71960d
Andrea Medina 07 <andreaaliciamedinaeverardo@gmail.com>
2024-10-11 18:13:03 -0600

    Práctica 4 finalizada

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+#Rutas para trabajar con un controlador
+Route::get('/',[controladorVistas::class,'home'])->name('rutainicio');
+
+Route::get('/formulario',[controladorVistas::class,'formulario'])->name('rutaformulario');
+
+Route::get('/clientes',[controladorVistas::class,'consulta'])->name('rutaclientes');
+
 
 /* Route::get('/', function () {
     return view('welcome');
 });  
 para poder comentar más rápido es shift,alt,a */ 
 
+
+/*
 #tipo de ruta view 
 Route::view('/','inicio')->name('rutainicio');
 #ruta y vista que se va a abrir
@@ -15,3 +26,4 @@ Route::view('/formulario','formulario')->name('rutaformulario');
 Route::view('/clientes','clientes')->name('rutaclientes');
 
 Route::view('/componentes','componentes')->name('rutacomponentes');
+*/
