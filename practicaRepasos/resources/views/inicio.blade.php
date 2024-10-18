fabe9d52862e75865d2e474e011801c7989c1d4d
Andrea Medina 07 <andreaaliciamedinaeverardo@gmail.com>
2024-10-18 17:33:49 -0600

    Ingreso y modificación de la interfaz de inicio

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/js/app.js'])
+
+    <style>
+        .image-container {
+            display: flex;
+            justify-content: center; /* Centrar para la parte horizontal */
+            align-items: center;     /* Centrar para la parte vertical */         
+            margin-top: 25px;
+            margin-bottom: 25px;
+        }
+
+        .boton{
+            display: flex;
+            justify-content: center; /* Centrar para la parte horizontal */
+            align-items: center;     /* Centrar para la parte vertical */         
+            margin-top: 25px;
+            margin-bottom: 25px;
+        }
+    </style>
+</head>
+<body>
+
+        <div class="image-container ">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo upq" width="180" height="180" >
+        </div>
+
+        <p style="text-align: center;" class="fs-5">Universidad Politécnica de Querétaro</p>
+        <p style="text-align: center;" class="fs-5">Ingeniería en Sistemas Computacionales</p>
+        <p style="text-align: center;" class="fs-5">Programación Web</p>
+        <p style="text-align: center;" class="fs-5">Repaso Laravel 1</p>
+        <p style="text-align: center;" class="fs-5">Alumna Andrea Alicia Medina Everardo</p>
+        <p style="text-align: center;" class="fs-5">Grupo S-195</p>
+        <p style="text-align: center;" class="fs-5">Séptimo cuatrimestre</p>
+        <div class="boton">
+            <a href="{{ route('rutaCalcula') }}" class="btn btn-warning">Repaso 1</a>
+        </div>
+        
+</body>
+</html>
--- practicaRepasos/routes/web.php
+++ practicaRepasos/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorCalcular; // Asegúrate de usar el controlador correcto
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Ruta para la vista principal
+Route::view('/', 'inicio')->name('rutainicio');
+// Ruta para la vista para la calculadora
+Route::view('/calcula', 'calcula')->name('rutaCalcula');
+
+// Ruta para el método convertir
+Route::post('/convertir', [ControladorCalcular::class, 'convertir'])->name('convertir');
