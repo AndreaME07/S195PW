aa857c0c41d1905a1f3273a669a578ca9eb74e72
Andrea Medina 07 <andreaaliciamedinaeverardo@gmail.com>
2024-10-31 18:52:51 -0600

    Creación, ajustes de elemetos, traducción de elementos

--- Repaso2/resources/views/formulario.blade.php
+++ Repaso2/resources/views/formulario.blade.php
@@ -1,50 +1,81 @@
 @extends('layouts.plantilla1')
+
 @section('contenido')
 
+
 <div class="container mt-4 p-5 m-5 rounded" style="background-color: rgb(220, 228, 245)"" >
-    <h1 class="display-6 text-center mb-4">Registro de Libros</h1>
-        <form action="#">
+    <h1 class="display-6 text-center mb-4">{{ __('Registro de Libros')}}</h1>
+
+    @if(session('exito'))
+        <script>
+            alertify.alert().set('message', '{{ session('exito') }}').show();
+        </script>
+    @endif
+        <form action="{{ route('procesar') }}" method="POST">
         <!--LLave de paso para envios por post -->
         @csrf
             <div class="row">
                 <div class="col-sm-6">
-                    <label for="nombre" class="form-label">ISBN</label>
-                    <input type="text" class="form-control" name="txtisbn">
+                    <label for="isbn" class="form-label" >{{__('ISBN')}}</label>
+                    <input type="text" class="form-control" name="txtisbn" value="{{ old('txtisbn')}}" >
+                    @error('txtisbn')
+                    <small style=" color: red"> {{ $errors->first('txtisbn') }}</small>
+                    @enderror
                 </div>
                 <div class="col-sm-6">
-                    <label for="titulo" class="form-label">Título</label>
-                    <input type="text" class="form-control" name="txttitulo">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6">
-                    <label for="autor" class="form-label">Autor</label>
-                    <input type="text" class="form-control" name="txtautor">
-                </div>
-                <div class="col-sm-6">
-                    <label for="paginas" class="form-label">Páginas</label>
-                    <input type="number" class="form-control" name="txtpaginas">
+                    <label for="titulo" class="form-label" >{{__('Título')}}</label>
+                    <input type="text" class="form-control" name="txttitulo" value="{{ old('txttitulo')}}" >
+                    @error('txttitulo')
+                    <small style=" color: red"> {{ $errors->first('txttitulo') }}}</small>
+                    @enderror
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-6">
-                    <label for="anio" class="form-label">Año</label>
-                    <input type="number" class="form-control" name="txtanio">
+                    <label for="autor" class="form-label" >{{__('Autor')}}</label>
+                    <input type="text" class="form-control" name="txtautor" value="{{ old('txtautor')}}">
+                    @error('txtautor')
+                    <small style=" color: red"> {{ $errors->first('txtautor') }}</small>
+                    @enderror
                 </div>
                 <div class="col-sm-6">
-                    <label for="editorial" class="form-label">Editorial</label>
-                    <input type="text" class="form-control" name="txteditorial">
+                    <label for="paginas" class="form-label" >{{__('Páginas')}}</label>
+                    <input type="text" class="form-control" name="txtpaginas" value="{{ old('txtpaginas')}}" >
+                    @error('txtpaginas')
+                    <small style=" color: red"> {{ $errors->first('txtpaginas') }} </small>
+                    @enderror
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <label for="anio" class="form-label" >{{__('Año')}}</label>
+                    <input type="text" class="form-control" name="txtanio" value="{{ old('txtanio')}}">
+                    @error('txtanio')
+                    <small style=" color: red"> {{ $errors->first('txtanio') }}</small>
+                    @enderror
+                </div>
+                <div class="col-sm-6">
+                    <label for="editorial" class="form-label" >{{__('Editorial')}}</label>
+                    <input type="text" class="form-control" name="txteditorial" value="{{ old('txteditorial')}}">
+                    @error('txteditorial')
+                    <small style=" color: red"> {{ $errors->first('txteditorial') }} </small>
+                    @enderror
                 </div>
             </div>
             <div class="col-12">
-                <label for="email" class="form-label">Email de la editorial</label>
-                <input type="email" class="form-control" name="txtemail">
+                <label for="email" class="form-label">{{__('Email de editorial')}}</label>
+                <input type="txt" class="form-control" name="txtemail" value="{{ old('txtemail')}}">
+                @error('txtemail')
+                    <small style=" color: red"> {{ $errors->first('txtemail') }} </small>
+                    @enderror
             </div>
             <div class="col-6 my-4 ">
-                <button type="submit" class="btn btn-outline-info btn-sm"> Guardar registro </button>
+                <button type="submit" class="btn btn-outline-info btn-sm">{{__('Guardar registro')}}</button>
             </div>
         </div>
         </form>
+
+        
 </div>
 
 @endsection
