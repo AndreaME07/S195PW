ffe176b5f36a2d4e9e851860b24555d0562e8954
Andrea Medina 07 <andreaaliciamedinaeverardo@gmail.com>
2024-11-04 18:05:54 -0600

    avance de las 6:05

---
+++ 2doP/app/Http/Controllers/ControladorFrituras.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControladorFrituras extends Controller
+{
+    
+public function abrirFormulario(){
+
+ 
+}
+
+public function formFrituras()
+    {
+        return view('formFrituras');
+    }
+
+
+}
---
+++ 2doP/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+/* Route::get('/', function () {
+    return view('form');
+});*/
+
+Route::view('/', 'formFrituras')->name('rutafrituras');
+
+
